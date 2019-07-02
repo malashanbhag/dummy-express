1073207e453f8c6616a45691b2abdff8463d26a9
Mala <mshanbhag@falabella.cl>
2019-07-02 15:12:23 +0530

    port change

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(8080, function () {
+const port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
     console.log("app running on port.", server.address().port);
 });
